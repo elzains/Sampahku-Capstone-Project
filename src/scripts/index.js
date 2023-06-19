533e4e4a577ab9b5aaedb93341a722d5cefdc990
Zainal Arifin <128154324+elzains@users.noreply.github.com>
2023-06-19 10:51:11 +0700

    Delete src directory

--- src/scripts/index.js
+++
@@ -1,7 +0,0 @@
-import 'regenerator-runtime';
-import '../styles/style.css';
-import bootstrap from 'bootstrap';
-import HeaderApp from './components/app-bar';
-import Jumbotron from './components/jumbotron';
-import RecylatorApp from './components/recylator';
-import FooterApp from './components/footer';
